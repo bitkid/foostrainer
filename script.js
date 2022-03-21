590c5778d5aaa50c8779110c48359491b72fb11b
bitkid <kid@bitkid.com>
2022-03-21 21:41:23 +0100

    use url params for training goals

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-let playButton = document.querySelector('#play');
-let stopButton = document.querySelector('#stop');
-let goalDelay = document.querySelector('#goal-delay');
-let goalDelayValue = document.querySelector('.goal-delay-value');
+const playButton = document.querySelector('#play');
+const stopButton = document.querySelector('#stop');
+const goalDelay = document.querySelector('#goal-delay');
+const passesDiv = document.querySelector('#passes');
+const shotsDiv = document.querySelector('#shots');
+const goalDelayValue = document.querySelector('.goal-delay-value');
 let routine = null;
 
 function setup() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let fiveBarGoals = urlParams.getAll("five");
+    if (fiveBarGoals.length === 0) {
+        fiveBarGoals = ["wall pass", "lane pass", "center pass"];
+    }
+    let threeBarGoals = urlParams.getAll("three")
+    if (threeBarGoals.length === 0) {
+        threeBarGoals = ["long left", "short left", "straight", "short right", "long right", "cut back left", "cut back right"];
+    }
+    console.log(fiveBarGoals);
+    console.log(threeBarGoals);
+    routine = new FoosballRoutine(fiveBarGoals, threeBarGoals, 5);
+
     document.getElementById("stop").disabled = true;
-    routine = new FoosballRoutine(["wall pass", "lane pass", "center pass"],
-        ["long left", "short left", "straight", "short right", "long right", "cut back left", "cut back right"],
-        5);
+    passesDiv.innerHTML = "<p>Passes: " + routine.fiveBarGoals.join(", ") + "</p>";
+    shotsDiv.innerHTML = "<p>Shots: " + routine.threeBarGoals.join(", ") + "</p>";
 }
 
 window.onload = setup;
