8fc974a08bc73ea09a9b36d35179ccd1daa6ab6b
sascha <sascha.sadat-guscheh@scinteco.com>
2023-11-11 23:45:16 +0100

    deploy directly to music

--- music/svelte/svelte.config.js
+++ music/svelte/svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 
     kit: {
         paths: {
-            base: '/music/build'
+            base: '/music'
         },
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
