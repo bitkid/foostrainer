c9785821fa825c6b02b11a675ee79804df5dc528
sascha <sascha.sadat-guscheh@scinteco.com>
2024-02-13 00:11:26 +0100

    remove unused tests

--- src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {describe, it} from 'vitest';
-import {FoosballRoutine} from "$lib/FoosballRoutine";
-
-describe('sum test', () => {
-    it('adds 1 + 2 to equal 3', () => {
-        const r = new FoosballRoutine()
-        r.start()
-    });
-});
