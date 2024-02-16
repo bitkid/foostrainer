94751c4512508ecde48e5cd022f9c5db69bb2e14
sascha <sascha.sadat-guscheh@scinteco.com>
2024-02-16 01:12:08 +0100

    done!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,4 @@
 <script>
     import "../app.pcss";
-    import {Navbar, NavBrand} from 'flowbite-svelte';
 </script>
-<Navbar title="yo">
-    <NavBrand href="/">
-        <span class="whitespace-nowrap font-semibold dark:text-white text-2xl">Foostrainer</span>
-    </NavBrand>
-</Navbar>
-
 <slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import {allPasses, allShots, FoosballRoutine} from "$lib/FoosballRoutine";
-    import {Accordion, AccordionItem, Button, Checkbox, Img, Label, Range} from 'flowbite-svelte'
+    import {Button, Card, Checkbox, Img, Label, Range} from 'flowbite-svelte'
     import {onMount} from "svelte"
+    // @ts-ignore
+    import EasySpeech from "easy-speech"
 
     let passes: string[] = ["1", "2", "3"]
     let shots: string[] = ["1", "2", "3", "4", "5", "6", "7"]
 
-    let goalDelay = 2
+    let ballResetTime = 2
+    let timeToSecondTouch = 2
+    let passExecutionTime = 2
+    let shotExecutionTime = 2
+    let ballSetupTime = 2
 
     let startButtonDisabled = false
     let stopButtonDisabled = true
@@ -16,18 +22,24 @@
     let status = "Start training!"
 
     $: {
-        if (routine)
-            routine.resetTime = goalDelay
+        if (routine) {
+            routine.timeUntilSecondTouch = timeToSecondTouch
+            routine.passExecutionTime = passExecutionTime
+            routine.ballSetupTime = ballSetupTime
+            routine.shotExecutionTime = shotExecutionTime
+            routine.ballResetTime = ballResetTime
+        }
     }
 
-    onMount(() => {
+    onMount(async () => {
+        await EasySpeech.init({maxTimeout: 5000, interval: 250})
         routine = new FoosballRoutine(passes, shots, (s: string) => {
             status = s
         })
     })
 
-    function start() {
-        routine.start()
+    async function start() {
+        await routine.start()
         startButtonDisabled = true
         stopButtonDisabled = false
     }
@@ -40,21 +52,20 @@
     }
 </script>
 
-<div class="p-5">
-    <Accordion>
-        <AccordionItem open>
-            <span slot="header">Train</span>
+<div class="p-1 justify-center content-center w-full">
+    <Card class="grid md:grid-cols-2" size="xl">
+        <div class="p-4">
+            <Label class="text-3xl pb-4 content-center">Foos trainer</Label>
             <ul class="pb-2 list-none">
                 <li>Put the ball into play on your five bar like you would for a normal match.</li>
                 <li>Optionally configure passes and shots you want to train.</li>
             </ul>
-            <Label class="text-xl pb-4 pt-4">{@html status}</Label>
+            <Label class="text-xl pb-4 pt-4 content-center">{@html status}</Label>
             <Button disabled={startButtonDisabled} on:click={() => start()}>Start</Button>
             <Button disabled={stopButtonDisabled} on:click={() => stop()}>Stop</Button>
-            <Img alt="table" caption="Passes and shots" class="pt-4" src="images/table.jpg"/>
-        </AccordionItem>
-        <AccordionItem>
-            <span slot="header">Configure</span>
+            <Img alt="table" caption="Passes and shots" class="pt-10" src="images/table.jpg"/>
+        </div>
+        <div class="p-4">
             <Label><p class="font-bold">Passes:</p></Label>
             <ul class="pb-2">
                 {#each allPasses as s}
@@ -71,8 +82,20 @@
                     </li>
                 {/each}
             </ul>
-            <Label><p class="font-bold">Goal reset delay:</p> {goalDelay} sec</Label>
-            <Range bind:value={goalDelay} max={10} min={0}>Range</Range>
-        </AccordionItem>
-    </Accordion>
+            <Label><p class="font-bold">Time until 2nd touch:</p> {timeToSecondTouch} sec</Label>
+            <Range bind:value={timeToSecondTouch} class="w-2/3" max={5} min={1}>Range</Range>
+
+            <Label><p class="font-bold">Pass execution time:</p> {passExecutionTime} sec</Label>
+            <Range bind:value={passExecutionTime} class="w-2/3" max={5} min={1}>Range</Range>
+
+            <Label><p class="font-bold">Ball setup time:</p> {ballSetupTime} sec</Label>
+            <Range bind:value={ballSetupTime} class="w-2/3" max={5} min={1}>Range</Range>
+
+            <Label><p class="font-bold">Shot execution time:</p> {shotExecutionTime} sec</Label>
+            <Range bind:value={shotExecutionTime} class="w-2/3" max={5} min={1}>Range</Range>
+
+            <Label><p class="font-bold">Ball reset time:</p> {ballResetTime} sec</Label>
+            <Range bind:value={ballResetTime} class="w-2/3" max={10} min={1}>Range</Range>
+        </div>
+    </Card>
 </div>
